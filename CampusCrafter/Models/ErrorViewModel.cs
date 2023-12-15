73111382c413cc1eb30ebdfc72ea82e233b67f34
Monczak <szymon.jedrzejczak.sj@gmail.com>
2023-12-15 16:02:05 +0100

    Initial commit

---
+++ CampusCrafter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CampusCrafter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
