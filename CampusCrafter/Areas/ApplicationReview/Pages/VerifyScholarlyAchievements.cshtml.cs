257d562159755e421cde4c31b321d57029a9c485
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-01-25 15:35:03 +0100

    Remove redundant FIXME

--- CampusCrafter/Areas/ApplicationReview/Pages/VerifyScholarlyAchievements.cshtml.cs
+++ CampusCrafter/Areas/ApplicationReview/Pages/VerifyScholarlyAchievements.cshtml.cs
@@ -67,7 +67,6 @@ public class VerifyScholarlyAchievements(ApplicationDbContext context, UserManag
         if (application is null)
             return NotFound();
         
-        // FIXME: This adds new records for some reason
         application.Applicant.ScholarlyAchievements = ScholarlyAchievements;
         context.CandidateApplications.Update(application);
         await context.SaveChangesAsync();
